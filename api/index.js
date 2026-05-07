be8aed2aa432dd03be959e45b903ad460e7b6664
cyrexvibes <mosesbenjamin1985@gmail.com>
2026-05-07 22:47:21 +0100

    Refactor index.js to export app module
    Refactor server code to export app for testing.

--- api/index.js
+++ api/index.js
@@ -29,4 +29,4 @@ app.post('/send-data', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
